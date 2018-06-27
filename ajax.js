efeb42624c1e7265361f023735e9e3de2d55fc5b
jeremy <jeremymartinsio1@gmail.com>
2018-06-27 15:40:01 +0200

    before merge

--- ajax.js
+++ ajax.js
@@ -167,15 +167,44 @@ function loadCmsStats() {
             var result = {};
             $.each(data, function (idx, elem) {
                 datasets.push(elem.count);
-                labels.push(elem._id.name);
+                labels.push(elem._id.filtre);
             })
             result.datasets = datasets;
             result.labels = labels;
             var ctx = $('#cmsChart');
+            console.log(result);
             if(result !== false) {
                 const data = {
                     datasets: [{
-                        data: result.datasets
+                        label: 'Répartition des CMS',
+                        data: result.datasets,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(78, 162, 47, 0.2)',
+                            'rgba(96, 63, 36, 0.2)',
+                            'rgba(12, 94, 147, 0.2)',
+                            'rgba(54, 130, 96, 0.2)',
+                            'rgba(20, 16, 145, 0.2)'
+
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(78, 162, 47, 1)',
+                            'rgba(96, 63, 36, 1)',
+                            'rgba(12, 94, 147, 1)',
+                            'rgba(54, 130, 96, 1)',
+                            'rgba(20, 16, 145, 1)'
+                        ]
                     }],
                     labels: result.labels
                 };
