d453d3cb4cb2fdc3340dac41fd4e323e0c58ac77
Valentin Dérudet <vderudet@studiosystems.fr>
2018-06-27 10:31:09 +0200

    ajax

--- ajax.js
+++ ajax.js
@@ -1,13 +1,13 @@
 function envoieAjax(param)
 {
     param['url'] = "http://192.168.43.238:8888/ProjetMongo/src/routeur/"+param['url'];
+    param['dataType'] = "json";
     $.ajax(param);
 }
 
 function getAllData()
 {
     envoieAjax({
-        dataType: 'json',
         type: 'GET',
         url: "allData",
         success: function(data) {
