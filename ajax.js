98884a1a7641a3eb89c851fcdb907e35a32b6d02
Valentin Dérudet <vderudet@studiosystems.fr>
2018-06-27 12:33:04 +0200

    hophop

--- ajax.js
+++ ajax.js
@@ -29,15 +29,28 @@ function createData()
 {
     $('#cache').toggleClass('dnone');
 
+    var data = {};
+
+    $('#formAjout').serializeArray().map(function(x) {
+        var res = x.name.split('$$');
+        if(typeof data[res[0]] === "undefined") {
+            data[res[0]] = {};
+        }
+        data[res[0]][res[1]] = x.value
+    });
+
     envoieAjax({
         type: 'POST',
         url: "newData",
         data: {
-            datas: $('#formAjout').serializeArray()
+            datas: JSON.stringify(data)
         },
         success: function(data) {
             if(parseInt(data.status) !== 1) {
                 alert('Error '+data.status+' : '+data.message);
+            }
+            else {
+                alert('Success : '+data.message);
             }
         },
         complete: function () {
